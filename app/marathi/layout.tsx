a33ca72630047dc55bdd04d9375346e36fbd7f95
krishnanshu <krishanshubhandarwar@gmail.com>
2026-08-12 17:24:04 +0530

    shriya

--- app/marathi/layout.tsx
+++ app/marathi/layout.tsx
@@ -1,11 +1,11 @@
 export const metadata = {
-  title: "डॉ. क्रिश्नांशू आणि डॉ. श्रिया यांचा विवाह सोहळा",
+  title: "डॉ. कृष्णांशू आणि डॉ. श्रिया यांचा विवाह सोहळा",
   description: "आमच्या आनंदात सहभागी होण्याचे अगत्याचे निमंत्रण.",
   openGraph: {
-    title: "डॉ. क्रिश्नांशू आणि डॉ. श्रिया यांचा विवाह सोहळा",
+    title: "डॉ. कृष्णांशू आणि डॉ. श्रिया यांचा विवाह सोहळा",
     description: "आमच्या नव्या प्रवासाची सुरुवात! ✨",
     url: 'https://ewedding-card-zeta.vercel.app/marathi', 
-    siteName: 'क्रिश्नांशू आणि श्रिया',
+    siteName: 'कृष्णांशू आणि श्रिया',
     images: [{ url: '/avatars/welcome.jpeg', width: 800, height: 600 }],
     locale: 'mr_IN',
     type: 'website',
